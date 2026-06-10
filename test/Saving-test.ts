5b3171372223e9cefb35d98b3ab22d9ad48aaf7b
tienphami01 <tien.phami2004@gmail.com>
2026-06-10 12:57:11 +0700

    import ethers

--- test/Saving-test.ts
+++ test/Saving-test.ts
@@ -1,16 +1,10 @@
 import {expect} from "chai";
-import hre from "hardhat";
-const { ethers } = hre;
-
-async function increaseTime(seconds: number) {
-    await hre.network.provider.send("evm_increaseTime", [seconds]);
-    await hre.network.provider.send("evm_mine");
-
-}
+import {ethers } from "hardhat";
+import { time }  from "@nomicfoundation/hardhat-network-helpers";
 const SECONDS_PER_DAY = 86400;
 
 describe("Blockchain Saving System", function () {
-    // biến dùng chung cho tất cả các tes;
+    // biến dùng chung cho tất cả các test;
     let mockUSDC: any;
     let vaultManager: any;
     let savingCore: any;
@@ -19,6 +13,7 @@ describe("Blockchain Saving System", function () {
     let bob: any;
     let feeReceiver: any;
 
+    // chạy trước mỗi test, đảm bảo mỗi test băt đầu với state sạch
     beforeEach(async function() {
         // lấy danh sách account từ hardhat
         [owner, alice, bob, feeReceiver] = await ethers.getSigners();
@@ -69,9 +64,11 @@ describe("Blockchain Saving System", function () {
     });
 
 
+      // createPlan 
+
     describe("createPlan", function() {
         it("Tạo plan hợp lệ ", async function() {
-            const plan = await savingCore.getPlan(0);
+            const plan = await savingCore.getPlan(0); // planId bắt đầu từ 0
             expect(plan.tenorDays).to.equal(90n);
             expect(plan.aprBps).to.equal(800n);
             expect(plan.enabled).to.equal(true);
@@ -97,6 +94,9 @@ describe("Blockchain Saving System", function () {
         
     });
 
+
+    // openDeposit
+
     describe("openDeposit", function() {
         it ("Mở deposit thành công - mint NFT cho alice", async function(){
             const amount = 1_000n *10n **6n;
@@ -117,8 +117,6 @@ describe("Blockchain Saving System", function () {
             const dep = await savingCore.getDeposit(0);
             expect(dep.pricipal).to.equal(amount);
             expect(dep.aprBpsAtOpen).to.equal(800n);
-
-
         });
 
         it("Token được chuyển vào contract", async function(){
@@ -146,10 +144,11 @@ describe("Blockchain Saving System", function () {
             ).to.be.reverted;
 
         });
-        it(">minDeposit bị reject", async function() {
+        it(" dưới minDeposit bị reject", async function() {
             // tạo plan có minDeposit = 500 USDC
 
-            await savingCore.createPlan(90,800,500n*10n**6n,0,500);
+            await savingCore.createPlan(90, 800, 500n * 10n ** 6n,0 , 500);
+            const amount = 100n * 10n ** 6n;
 
             await mockUSDC.connect(alice).approve(
                 await savingCore.getAddress(), amount
@@ -160,7 +159,9 @@ describe("Blockchain Saving System", function () {
 
         });
 
+    });
 
+    // withdrawAtEnd
         describe("withdrawAtEnd", function () {
             // mở 1 deposit 
             async function aliceOpenDeposit() {
@@ -170,13 +171,12 @@ describe("Blockchain Saving System", function () {
                 );
                 await savingCore.connect(alice).openDeposit(0, amount);
 
-
             }
 
             it("Rút đúng hạn", async function() {
                 await aliceOpenDeposit();
 
-                await increaseTime(9 * SECONDS_PER_DAY);
+                await time.increase(9 * SECONDS_PER_DAY);
 
                 const balanceBefore = await mockUSDC.balanceOf(alice.addtess);
                 await savingCore.connect(alice).withdrawAEnd(0);
@@ -196,7 +196,7 @@ describe("Blockchain Saving System", function () {
 
             it("rút trước hạn phải revert", async function() {
                 await aliceOpenDeposit();
-                await increaseTime(30 * SECONDS_PER_DAY);
+                await time.increase(30 * SECONDS_PER_DAY);
 
                 await expect(
                     savingCore.connect(alice).withdrawAtEnd(0)
@@ -206,7 +206,7 @@ describe("Blockchain Saving System", function () {
 
             it("Rút 2 lần phải revert lần 2", async function(){
                 await aliceOpenDeposit();
-                await increaseTime(90 * SECONDS_PER_DAY);
+                await time.increase(90 * SECONDS_PER_DAY);
                 await savingCore.connect(alice).withdrawAtEnd(0);
 
                 // lần 2 phải fail vì đã rút rồi
@@ -231,7 +231,7 @@ describe("Blockchain Saving System", function () {
             await savingCore.connect(alice).openDeposit(0, amount);
 
             // chưa đáo hạn
-            await increaseTime(30 * SECONDS_PER_DAY);
+            await time.increase(30 * SECONDS_PER_DAY);
 
             const aliceBefore = await mockUSDC.balanceOf(alice.address);
             const feeBefore = await mockUSDC.balanceOf(feeReceiver.address);
@@ -252,6 +252,157 @@ describe("Blockchain Saving System", function () {
         });
     });
     
-        
+    // renewDeposit
+    describe("renewDeposit", function() {
+        it("Gia hạn thủ công", async function () {
+            const amount = 1_000n * 10n ** 6n;
+            await mockUSDC.connect(alice).approve(
+                await savingCore.getAddress(), amount
+        );
 
-}
+        await savingCore.connect(alice).openDeposit(0, amount);
+        await time.increase(90 * SECONDS_PER_DAY);
+
+        await savingCore.connect(alice).renewDeposit(0,0);
+
+        // deposit mới 
+        const newDep = await savingCore.getDeposit(1);
+        const interest = (amount * 800n * 90n * 86400n) / (365n * 86400n *10_000n);
+        expect(newDep.principal).to.equal(amount + interest);
+        expect(newDep.status).to.equal(0n);
+
+    });
+    
+       it ("Deposit cũ status = ManualRenewed", async function() {
+        const amount = 1_000n * 10n ** 6n;
+        await mockUSDC.connect(alice).approve(
+            await savingCore.getAddress(), amount
+        );
+
+       await savingCore.connect(alice).openDeposit(0, amount);
+       await time.increase(90 * SECONDS_PER_DAY);
+       await savingCore.connect(alice).renewDeposit(0,0);
+       const oldDep = await savingCore.getDeposit(0);
+       expect(oldDep.status).to.equal(2n);
+       });
+    });
+
+
+    // autoRenewDeoposit
+
+    describe("autoRenewDeposit", function() {
+        async function aliceOpenDeposit() {
+            const amount = 1_000n * 10n ** 6n;
+            await mockUSDC.connect(alice).approve(
+                await savingCore.getAddress(), amount
+            );
+            await savingCore.connect(alice).openDeposit(0, amount);
+        }
+
+        it ("Auto renew trước đáo hạn phải revert", async function() {
+            await aliceOpenDeposit();
+            await time.increase(90 * SECONDS_PER_DAY);
+
+            await expect(
+                savingCore.autoRenewDeposit(0)
+            ).to.be.reverted;
+        });
+
+        it("Auto renew sau khi đáo hạn thành công", async function(){
+            await aliceOpenDeposit();
+            await time.increase(93 * SECONDS_PER_DAY + 1); // thời gian  gia hạn sau khi đáo hạn tối đa 3 ngày 
+        
+            const oldDep = await savingCore.getDeposit(0);
+            expect(oldDep.status).to.equal(3n);
+
+        });
+
+        it("APR được giữ nguyên sau khi auto renew", async function() {
+            await aliceOpenDeposit();
+            // Admin giảm lãi suất xuống còn 1%
+            await savingCore.updatePlan(0, 100);
+
+            await time.increase(93 * SECONDS_PER_DAY + 1);
+            await savingCore.autoRenewDeposit(0);
+
+            const newDep =await savingCore.getDeposit(1);
+            expect(newDep.aprBpsAtOpen).to.equal(800n);
+    
+        });
+
+    });
+
+    // vault
+
+    describe("Vault", function() {
+        it(" Vault tăng balance ", async function() {
+            const extra = 1_000n * 10n ** 6n;
+            await mockUSDC.approve(await vaultManager.getAddress(), extra);
+            await vaultManager.fundVault(extra);
+
+            expect(await vaultManager.vaultBalance())
+            .to.equal(101_000n *10n **6n);
+        });
+
+
+        it ("Vault không đủ tiền -> withdrawal revert", async function(){
+            // rút hết tiền của vault
+            await vaultManager.withdrawVault(await vaultManager.vaultBalance());
+            
+            const amount = 1_000n * 10n ** 6n;
+            await mockUSDC.connect(alice).approve(
+                await savingCore.getAddress(), amount
+            );
+            await savingCore.connect(alice).openDeposit(0, amount);
+            await time.increase(90 * SECONDS_PER_DAY);
+
+            await expect(
+                savingCore.connect(alice).withdrawAtEnd(0)
+            ).to.be.reverted;
+
+        });
+
+    
+    });
+
+
+    // Pause
+
+    describe("Pause", function() {
+        it("Không thể withdraw khi paused", async function() {
+            const amount = 1_000n * 10n ** 6n;
+            await mockUSDC.connect(alice).approve(
+                await savingCore.getAddress(), amount
+            );
+            await savingCore.connect(alice).openDeposit(0, amount);
+            await time.increase(90 * SECONDS_PER_DAY);
+
+            // addmin pause hệ thống
+            await savingCore.pause();
+
+            await expect(
+                savingCore.connect(alice).withdrawAtEnd(0)
+            ).to.be.reverted;
+
+        });
+        it ("có thể withdraw sau khi unpause", async function () {
+            const amount = 1_000n * 10n ** 6n;
+            await mockUSDC.connect(alice).approve(
+                await savingCore.getAddress(), amount
+            );
+            await savingCore.connect(alice).openDeposit(0, amount);
+            await time.increase(90 * SECONDS_PER_DAY);
+
+            await savingCore.pause();
+            await savingCore.unpause();
+
+            // 
+
+            await expect(
+                savingCore.connect(alice).withdrawAtEnd(0)
+            ).to.be.reverted;
+        });
+
+    });
+
+});
